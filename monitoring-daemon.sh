9a877b10ad0400fe67e292c31f76c2aee5d1b68f
Basyrov Rustam <hrustbas@gmail.com>
2025-05-16 16:39:54 +0300

    finish script logic

--- monitoring-daemon.sh
+++ monitoring-daemon.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
 
-set -e
-
 PROCESS_NAME="$1"
 PROCESS_PID=`pgrep --newest ${PROCESS_NAME}`
 PID_FILE="$1.pid"
+
+DATE=`date +%Y-%m-%dT%H:%M:%S`
 
 LOG_FILE="monitoring.log"
 
@@ -13,21 +13,21 @@ ENDPOINT="https://putsreq.com/kKddZBA0faGk0Lan61uy"
 
 CURL="curl"
 CURL_OPTS="-i -o /dev/null -X POST --max-time 2 -s -w %{http_code}"
+CURL_MSG="name=$PROCESS_NAME&pid=$PROCESS_PID&date=$DATE"
 
 if [ -n "${PROCESS_NAME}" ]; then
     if [ -f $PID_FILE ]; then
 	PREVIOUS_PID=`cat $PID_FILE`
 	if [ $PROCESS_PID != $PREVIOUS_PID ]; then
-	    echo "Процесс $PROCESS_NAME был перезапущен с PID=$PROCESS_PID" | tee -a $LOG_FILE
+	    echo "Процесс $PROCESS_NAME был перезапущен с PID=$PROCESS_PID. Date: $DATE" | tee -a $LOG_FILE
 	fi
     fi
     echo ${PROCESS_PID} > $PID_FILE
     
-    # There will be request
-    echo "Процесс $PROCESS_NAME с PID=$PROCESS_PID работает" | tee -a $LOG_FILE
-    # exec "$CURL_COMMAND"
-    response=`$CURL $CURL_OPTS -d "name=Pablo" $ENDPOINT`
-    echo $response
-    # echo $CURL_COMMAND
+    response=`$CURL $CURL_OPTS -d "$CURL_MSG" $ENDPOINT`
+    if [ $? -ne 0 ] || [ "$response" != "200" ]; then
+	MSG="Сервер $ENDPOINT не доступен. HTTP_CODE=$response"
+	echo $MSG | tee -a $LOG_FILE
+    fi
 fi
 
