46121e7ed1eddd8b78d890c969112a3788408700
rustam <rustam@honor-debian>
2025-05-18 15:58:04 +0300

    getopt

--- monitoring-daemon.sh
+++ monitoring-daemon.sh
@@ -1,34 +1,55 @@
 #!/bin/bash
 
-PROCESS_NAME="$1"
+USAGE_MSG="""\
+Usage: $0 -p [PROCESS NAME] -e [ENDPOINT]"""
+
+while getopts "hp:e:" OPT; do
+    case $OPT in
+	p)
+	    echo "Using PROCESS_NAME=$OPTARG"
+	    PROCESS_NAME="$OPTARG"
+	    ;;
+	e)
+	    echo "Using ENDPOINT=$OPTARG"
+	    ENDPOINT="$OPTARG"
+	    ;;
+	h)
+	    echo "$USAGE_MSG"
+	    exit 0
+	    ;;
+	\?)
+	    echo "Invalid option: $OPT"
+	    echo "$USAGE_MSG"
+	    exit 1
+    esac
+done
+
+if [ -z "$PROCESS_NAME" ]; then
+    PROCESS_NAME="test"
+    echo "Using default PROCESS_NAME=$PROCESS_NAME"
+fi
+
+if [ -z "$ENDPOINT" ]; then
+    ENDPOINT="https://test.com/monitoring/test/api"
+    echo "Using default ENDPOINT=$ENDPOINT"
+fi
+
+# exit 0
+
 PROCESS_PID=`pgrep --newest ${PROCESS_NAME}`
-PID_FILE="$1.pid"
+PID_FILE="$PROCESS_NAME.pid"
 
 DATE=`date +%Y-%m-%dT%H:%M:%S`
 
 LOG_FILE="monitoring.log"
 
-ENDPOINT="https://test.com/monitoring/test/api"
-ENDPOINT="https://putsreq.com/kKddZBA0faGk0Lan61uy"
+ENDPOINT="https://putsreq.com/gGkGyoIaz5GSGjWST1C4"
 
 CURL="curl"
 CURL_OPTS="-i -o /dev/null -X POST --max-time 2 -s -w %{http_code}"
 CURL_MSG="name=$PROCESS_NAME&pid=$PROCESS_PID&date=$DATE"
 
-while getopts ":p:e:" opt; do
-    case $opt in
-	p)
-	    echo "Using PROCESS_NAME=$opt"
-	    ;;
-	e)
-	    echo "Using ENDPOINT=$opt"
-	    ;;
-	\?)
-	    echo "Invalid option: $opt"
-    esac
-done
-
-if [ -n "${PROCESS_NAME}" ]; then
+if [ -n "${PROCESS_PID}" ]; then
     if [ -f $PID_FILE ]; then
 	PREVIOUS_PID=`cat $PID_FILE`
 	if [ $PROCESS_PID != $PREVIOUS_PID ]; then
